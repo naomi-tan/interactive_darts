d099b72978759fdc0f5a62ee84275d83c5b999d3
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-09-18 22:18:01 +0100

    feat: add circle detection using tutorial https://docs.opencv.org/4.x/da/d53/tutorial_py_houghcircles.html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
-
+import os
+import numpy as np
 
 def list_camera_indexes(limit = 10):
     # return valid camera indexes, limit 10
@@ -32,11 +33,29 @@ def capture_calibration_image(index = 0):
             break
 
         if cv.waitKey(1) == ord('c'):
-            cv.imwrite("calib/calibration_frame1.png", frame)
+            cv.imwrite("calib\\calibration_frame1.png", frame)
             break
 
     cap.release()
     cv.destroyAllWindows()
+
+
+def circle_detection():
+    # detect circles in calibration image
+    main_path = os.path.abspath(__file__)
+    img_path = os.path.join(os.path.dirname(main_path), "calib\\calibration_frame1.png")
+    print(img_path)
+
+    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) # Hough circle detection must be grayscale
+    cv.imshow('calibration image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    circles  = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+
+    for i in circles[0,:]:
+        print(i)
 
 
 def main():
@@ -45,7 +64,9 @@ def main():
     # camera_indexes = list_camera_indexes()
     # print(camera_indexes)
 
-    capture_calibration_image(0)
+    # capture_calibration_image(0)
+
+    circle_detection()
 
 
 if __name__ == "__main__":
