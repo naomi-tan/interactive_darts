61bfcbe5d5290fc8a9581ddd66f92e4b4bfe06d4
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-09-22 22:08:34 +0100

    feat: add blob detection function

--- main.py
+++ main.py
@@ -40,12 +40,8 @@ def capture_calibration_image(index = 0):
     cv.destroyAllWindows()
 
 
-def detect_circles(img_path):
+def detect_circles(img):
     # detect circles in calibration image
-    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) # Hough circle detection must be grayscale
-    img = cv.medianBlur(img, 5)
-    cv.imshow('calibration image', img)
-
     cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
 
     circles  = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 20, param1=100, param2=60, minRadius=0, maxRadius=0)
@@ -60,16 +56,9 @@ def detect_circles(img_path):
 
     cv.imshow('detected circles', cimg)
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 
-
-def fit_ellipse(img_path):
+def fit_ellipse(img):
     # fit ellipse to calibration image
-    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) # fit ellipse must be grayscale
-    h, w = img.shape[:2]
-    print(f'height: {h}, width: {w}')
-    img = cv.medianBlur(img, 5)
     ret, thresh = cv.threshold(img, 150, 255, 0) # 111/227 200
     contours, hierarchy = cv.findContours(thresh, 1, 2)
     cnt = contours[0]
@@ -82,8 +71,27 @@ def fit_ellipse(img_path):
     cv.imshow('ellipse image', img)
     print(ellipse)
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+
+def blob_detection(img):
+    # detect circles and ellipses in calibration image
+    params = cv.SimpleBlobDetector_Params()
+
+    # params.filterByArea = True
+    # params.minArea = 2000
+
+    # params.filterByConvexity = True
+    # params.minConvexity = 0.1
+
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
+
+    detector = cv.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(img)
+
+    blank = np.zeros((1, 1))
+    blobs = cv.drawKeypoints(img, keypoints, blank, (0, 0, 255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv.imshow('blob detection', blobs)
 
 
 def main():
@@ -98,11 +106,23 @@ def main():
     img_path = os.path.join(os.path.dirname(main_path), "calib\\calibration_frame1.png")
     print(img_path)
 
+    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)  # Hough circle detection must be grayscale
+    img = cv.medianBlur(img, 5)
+    cv.imshow('calibration image', img)
+
+    h, w = img.shape[:2]
+    print(f'height: {h}, width: {w}')
+
     # this is not a good way to detect dart board circles at an angle as they will be an ellipse
-    # detect_circles(img_path)
+    # detect_circles(img)
 
     # not sure how to set thresholds/parameters, not picking up the right ellipse
-    fit_ellipse(img_path)
+    # fit_ellipse(img)
+
+    blob_detection(img)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
